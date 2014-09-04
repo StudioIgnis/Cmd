c38d49fb6de768b72820aa038999c14b230e14cd
Luciano Longo <luciano.longo@gmail.com>
2014-09-03 22:40:35 -0300

    Diversify trait into multiple traits
    One generic and one Laravel specific that uses the generic one.

---
+++ src/CmdTrait.php
@@ -0,0 +1,33 @@
+<?php namespace StudioIgnis\Cmd;
+
+use Input, App;
+
+trait CmdTrait
+{
+    public function cmd($commandName, $input)
+    {
+        $params = [];
+
+        $class = new \ReflectionClass($commandName);
+
+        foreach($class->getConstructor()->getParameters() as $param)
+        {
+            $name = $param->getName();
+
+            if (array_key_exists($name, $input))
+            {
+                $params[] = $input[$name];
+            }
+            elseif($param->isDefaultValueAvailable())
+            {
+                $params[] = $param->getDefaultValue();
+            }
+            else
+            {
+                throw new \InvalidArgumentException("Couldn't find [$name] in input for command [$commandName]");
+            }
+        }
+
+        return $class->newInstanceArgs($params);
+    }
+}
--- src/Laravel/CmdTrait.php
+++ src/Laravel/CmdTrait.php
@@ -4,38 +4,13 @@ use Input, App;
 
 trait CmdTrait
 {
+    use \StudioIgnis\Cmd\CmdTrait;
+
     public function execute($commandName, array $input = null)
     {
         $this->getCommandBus()->execute(
             $this->cmd($commandName, $input ?: Input::all())
         );
-    }
-
-    public function cmd($commandName, $input)
-    {
-        $params = [];
-
-        $class = new \ReflectionClass($commandName);
-
-        foreach($class->getConstructor()->getParameters() as $param)
-        {
-            $name = $param->getName();
-
-            if (array_key_exists($name, $input))
-            {
-                $params[] = $input[$name];
-            }
-            elseif($param->isDefaultValueAvailable())
-            {
-                $params[] = $param->getDefaultValue();
-            }
-            else
-            {
-                throw new \InvalidArgumentException("Couldn't find [$name] in input for command [$commandName]");
-            }
-        }
-
-        return $class->newInstanceArgs($params);
     }
 
     protected function getCommandBus()
