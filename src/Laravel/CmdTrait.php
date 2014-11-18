35529bb36b6f1d98ff545e19fa23b9d72174d501
Luciano Longo <luciano.longo@gmail.com>
2014-11-18 20:13:11 -0300

    Add method to get the input
    This way it can be overridden if the user doesn't want to use Laravel's
    "facade" invocation

--- src/Laravel/CmdTrait.php
+++ src/Laravel/CmdTrait.php
@@ -12,10 +12,15 @@ trait CmdTrait
         if ($commandName instanceof Command) {
             $cmd = $commandName;
         } else {
-            $cmd = $this->cmd($commandName, $input ?: Input::all());
+            $cmd = $this->cmd($commandName, $this->getInput($input));
         }
 
         return $this->getCommandBus()->execute($cmd);
+    }
+
+    protected function getInput(array $input = null)
+    {
+        return $input ?: Input::all();
     }
 
     protected function getCommandBus()
